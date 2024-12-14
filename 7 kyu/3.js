e6ea5138e086ba217e889dfff2ee9dcbecbe7dd8
ReachTheEnd <reachtheendstudy@gmail.com>
2024-12-14 21:01:30 +0300

    add lesson 3

---
+++ 7 kyu/3.js
@@ -0,0 +1,9 @@
+/*
+
+1. https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+2. https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
+3. https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/javascript
+4. https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+5. https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+
+*/
